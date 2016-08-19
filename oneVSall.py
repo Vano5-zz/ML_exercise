96244e455935b5d3c6ebc29dd6735a2c44d4b66d
Vano5 <i.veremiichuk@gmail.com>
2016-08-19 16:12:59 +0300

    create new exercise

---
+++ oneVSall.py
@@ -0,0 +1,21 @@
+import sys 
+
+import PIL.Image 
+import scipy.misc, scipy.optimize, scipy.io, scipy.special
+import numpy as np
+ 
+import pylab 
+from matplotlib import pyplot, cm 
+from mpl_toolkits.mplot3d import Axes3D 
+import matplotlib.mlab as mlab 
+import pandas as pd
+
+
+
+#mat=scipy.io.loadmat ("F:\EPAM\coursera\ML_COURSERA_GARVARD\machine-learning-ex3\machine-learning-ex3\ex3\ex3data1.mat")
+#y=np.array([mat['y']])
+#x=np.array([mat['X']])
+
+
+
+print y
